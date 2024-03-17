cdedcd76d00920344e619983617facabb7bd3fa7
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-03-17 22:19:44 +0100

    removed unecessary files

--- machineLearningModel/Model/main.py
+++ machineLearningModel/Model/main.py
@@ -6,7 +6,7 @@ import pandas as pd
 # Load model and preprocessors
 model, mlb, scaler, class_names, diseases_encoded = load_model_and_preprocessors()
 
-input_symptoms = ['gut problems nausea belly pain diarrhea vomiting', 'breathing problems shortness breath serious respiratory distress failure pneumonia respiratory diseases', 'brain nervous system changes might notice shifts behavior thinking even organ function seizures possible serious cases']
+input_symptoms = ['swelling', 'redness', 'tenderness touch', 'pain', 'limited movement joint toe', 'corns calluses']
 # Predict diseases for the given symptoms
 predicted_diseases = predict_disease(mlb=mlb,scaler=scaler,model=model, symptoms=input_symptoms, class_names=class_names, diseases_encoded=diseases_encoded)
 
