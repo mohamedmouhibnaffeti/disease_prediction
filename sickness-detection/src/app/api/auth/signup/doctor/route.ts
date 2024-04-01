8fc8a1695dabf9f9a47d10f6fc4d57c29f988ca2
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-04-01 23:45:57 +0100

    create user endpoint

--- sickness-detection/src/app/api/auth/signup/doctor/route.ts
+++ sickness-detection/src/app/api/auth/signup/doctor/route.ts
@@ -1,15 +1,27 @@
-import connectMongoDB from "@/lib/mongodb";
-import { Admin, Doctor, Patient } from "@/Models/UserModel/UserModel";
-import { NextResponse } from "next/server";
+import { Doctor, User } from "@/Models/UserModel/UserModel"
+import { isValidEmail } from "@/lib/functions/strings"
+import connectMongoDB from "@/lib/mongodb"
+import { NextResponse } from "next/server"
 
-export async function POST(request: Request) {
+export async function POST(request: Request){
     try{
-        const {name, lastname, email, password, phone} = await request.json()
+        const { name, lastname, email, password, confirmPassword, id_images } = await request.json()
         await connectMongoDB()
-        const res = await Admin.create({name, lastname, email, password})
-        console.log(res)
-        return NextResponse.json({ message: "Sickness Created" }, { status: 201 })
+        if(name.length < 6 || lastname.length < 6 || !isValidEmail(email) || password.length < 6 || confirmPassword < 6 || confirmPassword !== password || id_images.length !== 2 ){
+            return NextResponse.json({ message: "Please check form data" }, { status: 400 })
+        }
+        const exisingDoctor = await User.findOne({email: email})
+        if(exisingDoctor){
+            return NextResponse.json({message: "Doctor already exists" }, { status: 400 })
+        }else{
+            const CreatedDoctor = await Doctor.create({ name, lastname, email, password, id_images })
+            if(CreatedDoctor){
+                return NextResponse.json({ message: "Doctor Created", doctor: CreatedDoctor }, { status: 201 })
+            }else{
+                return NextResponse.json({ message: "doctor not created" }, { status: 500 })
+            }
+        }
     }catch(err){
-        console.log(err)
+        return NextResponse.json({ message: "error signing up doctor", error: err }, { status: 500 })
     }
 }
