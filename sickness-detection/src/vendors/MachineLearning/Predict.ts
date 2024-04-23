f3b33154accc5df526ed6e786fbb29a1b33d80f2
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-04-23 22:41:39 +0100

    merged model with front

--- sickness-detection/src/vendors/MachineLearning/Predict.ts
+++ sickness-detection/src/vendors/MachineLearning/Predict.ts
@@ -1,4 +1,4 @@
-export default async ({Symptoms}: {Symptoms?: Array<string>}) => {
+export default async function Predict ({Symptoms}: {Symptoms?: Array<string>}){
     try {
         const PredictionResponse = await fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
@@ -12,6 +12,7 @@ export default async ({Symptoms}: {Symptoms?: Array<string>}) => {
 
         if (PredictionResponse.ok) {
             const data = await PredictionResponse.json();
+            console.log(data)
             return data.predicted_diseases
         } else {
             throw new Error('Failed to fetch');
