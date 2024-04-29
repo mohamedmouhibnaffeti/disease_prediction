4ad8eae9eff66cd0e603cc8f3839d0256f1c28ec
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-04-29 10:47:11 +0100

    handle server errors in fetch and create sickness

--- sickness-detection/src/app/api/Sickness/route.ts
+++ sickness-detection/src/app/api/Sickness/route.ts
@@ -2,16 +2,24 @@ import connectMongoDB from "@/lib/mongodb";
 import Sickness from "@/Models/SicknessModel/Sickness";
 import { NextResponse } from "next/server";
 export async function POST(request: Request) {
-    const { title, symptoms } = await request.json();
-    await connectMongoDB();
-    const res = await Sickness.create({ title: title, symptoms: symptoms });
-    return NextResponse.json({ message: "Sickness Created" }, { status: 201 });
+    try{
+        const { title, symptoms } = await request.json();
+        await connectMongoDB();
+        const res = await Sickness.create({ title: title, symptoms: symptoms });
+        return NextResponse.json({ message: "Sickness Created" }, { status: 201 });
+    }catch(err){
+        return NextResponse.json({ message: `Internal Server Error:${err}` }, { status: 500 })
+    }
 }
 
 export async function GET() {
-    await connectMongoDB();
-    const Sicknesses = await Sickness.find();
-    return NextResponse.json({ Sicknesses: Sicknesses });
+    try{
+        await connectMongoDB();
+        const Sicknesses = await Sickness.find();
+        return NextResponse.json({ Sicknesses: Sicknesses }, {status : 200});
+    }catch(err){
+        return NextResponse.json({ message: `Internal Server Error:${err}` }, { status: 500 })
+    }
 }
 
 // CORS preflight request handler
