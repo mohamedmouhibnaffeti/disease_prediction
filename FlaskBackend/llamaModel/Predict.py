a7fa0ace8ce6861cdd69730917d88776845161b8
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-05-05 08:58:44 +0100

    changed predict function to prevent buffer issues

--- FlaskBackend/llamaModel/Predict.py
+++ FlaskBackend/llamaModel/Predict.py
@@ -1,32 +1,33 @@
 from llama_cpp import Llama
 from transformers import AutoTokenizer
 # Replace 'bert-base-uncased' with the appropriate model name
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+def predict_disease_llama(symptoms_string):  
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-### Instruction:
-{}
+    alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
-### Input:
-{}
+    ### Instruction:
+    {}
 
-### Response:
-{}"""
+    ### Input:
+    {}
 
-EOS_TOKEN = tokenizer.eos_token
+    ### Response:
+    {}"""
 
-def formatting_prompts_func(examples):
-    instructions = examples["instruction"]
-    inputs       = examples["input"]
-    outputs      = examples["title"]
-    texts = []
-    for instruction, input, output in zip(instructions, inputs, outputs):
-        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN 
-        texts.append(text)
-    return { "text" : texts, }
+    EOS_TOKEN = tokenizer.eos_token
 
-def predict_disease_llama(symptoms_string):    
+    def formatting_prompts_func(examples):
+        instructions = examples["instruction"]
+        inputs       = examples["input"]
+        outputs      = examples["title"]
+        texts = []
+        for instruction, input, output in zip(instructions, inputs, outputs):
+            text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN 
+            texts.append(text)
+        return { "text" : texts, }
+  
     model_path = "C:/Users/mouha/OneDrive/Desktop/PFE/FlaskBackend/llamaModel/Copie de model-unsloth.Q4_K_M.gguf"
     llm = Llama(model_path=model_path)
     prompt = alpaca_prompt.format(
