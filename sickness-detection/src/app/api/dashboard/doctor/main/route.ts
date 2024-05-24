b1b880859dcadcdda70485240a8f8a8dbec08d68
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-05-24 09:08:44 +0100

    doctor main page appointments api

--- sickness-detection/src/app/api/dashboard/doctor/main/route.ts
+++ sickness-detection/src/app/api/dashboard/doctor/main/route.ts
@@ -1,13 +1,48 @@
 import { NextResponse, NextRequest } from "next/server";
 import connectMongoDB from "@/lib/mongodb";
 import Appointment from "@/Models/AppointmentModel/Appointment";
+import { User } from "@/Models/UserModel/UserModel";
 
 export async function GET(request: NextRequest){
     try{
-        const doctorID = (request.nextUrl.searchParams.get('doctorID') || "")
+        const doctorID = (request.nextUrl.searchParams.get('doctorID') || "660ad6cfb9de95e17ff96c90")
         connectMongoDB()
-        const doctorAppointments = await Appointment.find({doctor: doctorID})
-        return NextResponse.json({ message: doctorAppointments }, { status: 200 })
+        const MyAppointments = await Appointment.find({doctor: doctorID})
+        const Users = await User.find()
+        
+        const pendingAppointments: any = []
+        const AcceptedAppointments: any = []
+        const MyPatients: any = []
+        MyAppointments.some((appointment) => {
+            if(appointment.state === "pending"){
+                pendingAppointments.push(appointment)
+            }
+            if(appointment.state === "accepted"){
+                AcceptedAppointments.push(appointment)
+            }
+            if((!MyPatients.some(((existingAppointment: any) => existingAppointment["patient"] === appointment.patient)) && appointment.state === "accepted")){
+                MyPatients.push(appointment)
+            }
+        })
+
+        const totalPatients: any = []
+        const totalDoctors: any = []
+        Users.some((user) => {
+            if(user.role === "doctor"){
+                totalDoctors.push(user)
+            }
+            if(user.role === "patient"){
+                totalPatients.push(user)
+            }
+        })
+        return NextResponse.json({ 
+            acceptedAppointments: AcceptedAppointments.length,
+            pendingAppointments: pendingAppointments, 
+            totalAppointments: MyAppointments.length,
+            myPatients: MyPatients,
+            totalPatients: totalPatients.length,
+            totalDoctors: totalDoctors.length 
+        }, { status: 200 })
     }catch(err){
         return NextResponse.json({ message: `Internal server error: ${err}` }, { status: 500 })
     }
