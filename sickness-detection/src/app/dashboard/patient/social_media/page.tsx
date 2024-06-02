41d33d29fec9e7ff15beb255e2de60c63238a76d
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-06-02 23:56:13 +0100

    social media won't be rendered

--- sickness-detection/src/app/dashboard/patient/social_media/page.tsx
+++ sickness-detection/src/app/dashboard/patient/social_media/page.tsx
@@ -20,7 +20,26 @@ const PatientSocialStats = () => {
                     <PatientNavBarDash />
                     <main className="flex-1 p-4 md:p-6">
                     {
-                        !requestLoading?
+                        true 
+                        ?
+                        <div className="grid h-screen place-content-center bg-white px-4">
+                            <div className="text-center">
+                                <h1 className="text-9xl font-black text-gray-200">SORRY</h1>
+
+                                <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
+
+                                <p className="mt-4 text-gray-500">We're currently working on this page.</p>
+
+                                <a
+                                href="/"
+                                className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
+                                >
+                                Go Back Home
+                                </a>
+                            </div>
+                        </div>
+                        :
+                        (!requestLoading?
                         (
                             200 === 200 ?
                             <>
@@ -49,7 +68,7 @@ const PatientSocialStats = () => {
                         :
                         <div className="w-full h-full flex justify-center items-center">
                             <MainLoader />
-                        </div>
+                        </div>)
                     }
                     </main>
                 </div>
--- sickness-detection/src/app/social/page.tsx
+++ sickness-detection/src/app/social/page.tsx
@@ -2,11 +2,33 @@ import PostCard from "@/components/PostCard";
 
 export default function Social(){
     return(
-        <div className="w-full min-h-screen flex justify-center items-start bg-sickness-social">
-            <div className="md:px-8 py-4 px-4 border-sickness-border bo rounded-sm grid grid-cols-1 mt-[6rem] gap-4">
-                <PostCard />
-                <PostCard />
-            </div>
-        </div>
+        <>
+            {
+                false ?
+                <div className="w-full min-h-screen flex justify-center items-start bg-sickness-social">
+                    <div className="md:px-8 py-4 px-4 border-sickness-border bo rounded-sm grid grid-cols-1 mt-[6rem] gap-4">
+                        <PostCard />
+                        <PostCard />
+                    </div>
+                </div>
+                :
+                <div className="grid h-screen place-content-center bg-white px-4">
+                    <div className="text-center">
+                        <h1 className="text-9xl font-black text-gray-200">SORRY</h1>
+
+                        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
+
+                        <p className="mt-4 text-gray-500">We're currently working on this page.</p>
+
+                        <a
+                        href="/"
+                        className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
+                        >
+                        Go Back Home
+                        </a>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
