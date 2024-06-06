acc9fff4739dab8695c181db30fedad7ce29488d
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-06-06 03:07:05 +0100

    deploy fixed url

--- sickness-detection/src/lib/statics/ApiRoutes.ts
+++ sickness-detection/src/lib/statics/ApiRoutes.ts
@@ -1,12 +1,3 @@
 export const flask_backend_route = "http://127.0.0.1:5000"
-// utils/baseUrl.js
-
-export function getBaseUrl() {
-    if (typeof window !== 'undefined') {
-      return window.location.origin;
-    } else {
-      return "";
-    }
-  }
   
-export const next_backend_route = "http://localhost:3030/api"
+export const next_backend_route = process.env.next_backend_route
