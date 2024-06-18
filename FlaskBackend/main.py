1c3bf3b79f7e62fcc7c01e1d6a721533b7e3ae68
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-06-18 01:00:15 +0100

    scraper backend code

--- FlaskBackend/main.py
+++ FlaskBackend/main.py
@@ -16,26 +16,36 @@ from machineLearningModel.Model.predict import predict_top_sickness
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from flask_socketio import SocketIO
+import importlib
 
 
-def kill_thread():
-    global global_thread
-
-    if global_thread is None:
+def kill_thread(thread):
+    if thread is None:
         print("No thread to kill.")
         return
 
-    if global_thread.ident is None:
+    if thread.ident is None:
         print("Thread has not started.")
         return
 
-    tid = global_thread.ident
+    tid = thread.ident
     exc = ctypes.py_object(SystemExit)
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), exc)
     if res == 0:
         print("Failed to kill the thread.")
     else:
         print("Thread killed.")
+
+
+
+def scrape_selected_websites(selected_websites):
+    for scraper_info in settings.python_files:
+        if scraper_info['file name'] in selected_websites:
+            module = importlib.import_module(f"scrapers.{scraper_info['file name']}")
+            scraper_class = getattr(module, scraper_info['class name'])
+            scraper_instance = scraper_class(socketio)
+            scrape_function = getattr(scraper_instance, scraper_info['function name'])
+            scrape_function()
 
 def extract_file_names(files_list):
     file_names = set()
@@ -80,88 +90,48 @@ MayoClinic = MayoClinicScraper(socketio)
 @app.route('/api/running-status', methods=['GET'])
 def get_running_status():
     global running
+    print(running)
     return jsonify({"running": running})
 
-@app.route('/api/dataget', methods=['GET'])
+
+@app.route('/api/getWebsites', methods=['GET'])
 def send_scrapes():
-    x=settings.python_files
-    print(x)
-    return jsonify(extract_file_names(x))
+    file_names = set()
+    for file_dict in settings.python_files:
+        file_names.add(file_dict.get('file name'))
+    return jsonify(list(file_names))
+
 
 
 
 @app.route('/api/start-scraping', methods=['POST'])
 def start_collecting():
-    write_csv_header()
     global global_thread
     global running
     running = True
-    global_thread = threading.Thread(target=MayoClinic.main_MayoClinicScraper)
+    selected_websites = request.json.get('websites', [])
+    write_csv_header()
+    global_thread = threading.Thread(target=scrape_selected_websites, args=(selected_websites,))
     global_thread.start()
-    return jsonify('scraper completed !')
+    global_thread.join()
+    running = False
+
+    documentation_path = 'data.csv'
+    return send_file(documentation_path, as_attachment=True)
+
+
 
 
 
 @app.route('/api/stop-scraping', methods=['POST'])
 def stop_collecting():
+    global global_thread
     global running
     running = False
-    kill_thread()
+    kill_thread(global_thread)
     documentation_path = 'data.csv'
     return send_file(documentation_path, as_attachment=True)
 
-
-@app.route('/api/upload-scraper', methods=['POST'])
-def upload_scraper():
-    file = request.files['file']
-    class_name = request.form['className']
-    class_Function = request.form['classFunction']
-    x=settings.python_files
-
-    if find_file_or_class(file.filename[:(file.filename.rfind('.'))], class_name, x):
-        return 'Scraper file already exists.'
-    
-
-    
-    data= {"file name": file.filename[:(file.filename.rfind('.'))] , "class name": class_name, "function name": class_Function}
-    x.append(data)
-    with open('settings.py', 'w') as f:
-        f.write(f'python_files={x}')
-    file.save(os.path.join("./scrapers/", file.filename))
-    return 'File uploaded successfully'
-
-@app.route('/api/download-documentation')
-def download_documentation():
-    documentation_path = 'scrapers/book.py'
-    return send_file(documentation_path, as_attachment=True)
-
-@app.route('/api/delete-scraper', methods=['POST'])
-def delete_scraper_route():
-    try:
-        file_name = request.json['fileName']
-        python_files = settings.python_files
-        updated_files = [file for file in python_files if file['file name'] != file_name]
-        with open('settings.py', 'w') as f:
-            f.write(f'python_files={updated_files}')
-        scraper_path = os.path.join("./scrapers/", file_name + ".py")
-        os.remove(scraper_path)
-        print('Scraper deleted successfully')
-        return 'Scraper deleted successfully', 200
-    except Exception as e:
-        return str(e), 400
-
-
-@app.route('/api/modify-scraper', methods=['POST'])
-def modify_scraper():
-    file_name = request.json['file']
-    new_content = request.json['content']
-    try:
-        with open(os.path.join("./scrapers/", f'{file_name}.py'), 'w') as f:
-            f.write(new_content)
-        return 'Scraper modified successfully', 200
-    except Exception as e:
-        return str(e), 400
-    
 
 #end scraper code
 
